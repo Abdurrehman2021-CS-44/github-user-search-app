d97eefcc2617c94e9d4325aa6b86d75ef6c5744c
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-21 14:42:14 +0500

    Fetch data from the api

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import User from "./User";
 import NotFound from "./NotFound"
 
 const App = () => {
+    const [userData, setUserData] = useState({});
+    const [user, setUser] = useState("Abdurrehman2021-CS-44");
+
+    const getData = async () => {
+        const response = await fetch("https://api.github.com/users/"+user);
+        const data = await response.json();
+        setUserData(data);
+        console.log(userData)
+    }
+
     return <>
         <Header />
         <div className="search-bar">
-            <input className="search-input" type="text" placeholder="Enter username" />
+            <input className="search-input" type="text" placeholder="Enter username" value={user} onChange={handleChange} />
             <button className="search-btn"><i className="fa-solid fa-magnifying-glass fa-xl"></i></button>
         </div>
         <User />
